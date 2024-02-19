88875cb0b0bf8a5ea616b727171f3c802d7955c8
Pushp Vashisht <pushptyagi1@gmail.com>
2024-02-19 15:33:11 +0000

    Setup initial react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './components/homepage';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
