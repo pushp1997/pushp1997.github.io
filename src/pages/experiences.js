b7ca2b49b1e0ab24deac5073577dc9e0dc1c65c8
Pushp Vashisht <pushptyagi1@gmail.com>
2025-01-01 03:12:38 +0000

    Added structure and logic for Experiences Page

--- src/pages/experiences.js
+++ src/pages/experiences.js
@@ -1,6 +1,21 @@
-import { Box } from "@mui/material";
-import { Toolbar } from "@mui/material";
+import { Box, Toolbar } from "@mui/material";
 import { drawerWidth } from '../constants';
+import experiences from '../data/experiences';
+
+
+const JobEntry = ({ title, company, duration, location, responsibilities }) => (
+    <div>
+        <h2>{title}</h2>
+        <div className="w3-container w3-sand w3-leftbar" style={{marginBottom: '20px'}}>
+            <p><em>{company}<br />{duration}<br />{location}</em></p>
+            <ul>
+            {responsibilities.map((resp, index) => (
+                <li key={index}>{resp}</li>
+            ))}
+            </ul>
+        </div>
+    </div>
+);
 
 
 function ExperiencePage() {
@@ -10,7 +25,9 @@ function ExperiencePage() {
           sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
         >
             <Toolbar />
-            <div>Experience Page</div>
+            {experiences.map((job, index) => (
+                <JobEntry key={index} {...job} />
+            ))}
         </Box>
     );
 }
